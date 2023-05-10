558ee9b52c9235681779fc8a7ccfcbd49b1e88b0
leinn45 <lfm0032951@est.univalle.edu>
2023-05-10 14:16:32 -0400

    ver1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace git.hoy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num;
+            int num1;
+            Console.WriteLine("ingrese un numero");
+         
+
+        }
+    }
+}
